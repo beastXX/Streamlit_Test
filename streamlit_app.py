04bb6cb7dc9af430db1be7eb4cf75a92fccabf52
beastXX <37077642+beastXX@users.noreply.github.com>
2022-06-09 13:17:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.write("My First App. Hello world!")
+st.write("""
+# My First App
+Hello *world*
+""")
+
+x = st.slider("x")
+st.write(x, 'squared is', x * x)
+
+label = st.selectbox('Filter to:', ['A', 'B',])
+st.write(label)
