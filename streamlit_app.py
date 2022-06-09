4cea47a0df9f73b83bea93ac0ffe5891e0eae052
beastXX <37077642+beastXX@users.noreply.github.com>
2022-06-09 13:53:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
+import os
+
+cp = os.path.dirname(os.path.abspath(__file__))
 
 st.write("""
 # My First App
 Hello *world*
 """)
+
+st.write(cp)
 
 x = st.slider("x")
 st.write(x, 'squared is', x * x)
